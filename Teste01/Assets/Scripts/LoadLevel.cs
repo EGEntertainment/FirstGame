c9a5f176878d35662793e9eff4b9ba2313669acb
EGEntertainment <eletronicgeneration@gmail.com>
2015-06-26 07:37:50 -0300

    Teste de commit

---
+++ Teste01/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public int Level;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		//Application.LoadLevel ("Level1");	
+	}
+	
+	public void CarregaLevel (int Level){
+		Application.LoadLevel (Level);
+	}
+}
